ca7af883a2e76a41f3a6ecd766fe7b679f6e2603
aelledge <alexander.r.elledge@gmail.com>
2026-04-05 13:26:51 -0400

    Wrong bash format for variable. Oops.

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 docker_ip=$(docker inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aelledge-docker-sql)
 
-psql -U postgres-user -h $(docker_ip) --dbname=maindb -f ./initdb.sql
+psql -U postgres-user -h $docker_ip --dbname=maindb -f ./initdb.sql
