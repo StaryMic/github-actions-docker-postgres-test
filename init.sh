bbd84d44bde402d3b51b6e4ef57370d1acbc6112
aelledge <alexander.r.elledge@gmail.com>
2026-04-05 14:04:19 -0400

    Re-added the IP to the init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 docker_ip=$(docker inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aelledge-docker-sql)
 
-psql -U postgres-user --dbname=maindb -f ./initdb.sql
+psql -h=$docker_ip -U postgres-user --dbname=maindb -f ./initdb.sql
