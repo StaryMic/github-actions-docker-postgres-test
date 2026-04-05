b8245c984e58de576bb758c66758f39e437ada81
aelledge <alexander.r.elledge@gmail.com>
2026-04-05 14:42:17 -0400

    Changed from wait-on to pg_isready, as well as changing the script and removing a duplicate .sql file being ran.

--- dockerfile
+++ dockerfile
@@ -1,6 +1,5 @@
 FROM postgres:16
 WORKDIR /postgres
-COPY ./initdb.sql /docker-entrypoint-initdb.d/initdb.sql
 ENV POSTGRES_DB=maindb
 ENV POSTGRES_USER=postgres-user
 ENV POSTGRES_PASSWORD=password
--- init.sh
+++ init.sh
@@ -1,1 +1,3 @@
-psql -h localhost -U postgres-user --dbname=maindb -f ./initdb.sql
+docker_ip=$(docker inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aelledge-docker-sql)
+
+psql -h $docker_ip -U postgres-user --dbname=maindb -f ./initdb.sql
