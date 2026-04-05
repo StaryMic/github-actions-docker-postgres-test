a334632f006533108629978d02c7a5df9e5bfbbc
aelledge <alexander.r.elledge@gmail.com>
2026-04-05 14:24:09 -0400

    trying ip stuff again...

--- init.sh
+++ init.sh
@@ -1,3 +1,1 @@
-docker_ip=$(docker inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aelledge-docker-sql)
-
-psql -h $docker_ip -U postgres-user --dbname=maindb -f ./initdb.sql
+psql -h localhost -U postgres-user --dbname=maindb -f ./initdb.sql
